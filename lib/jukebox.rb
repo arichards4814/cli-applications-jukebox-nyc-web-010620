ebcf073da921af4d368469c241ed420b1347e83b
Andrew Richards <arichards4814@gmail.com>
2019-12-10 00:22:17 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,57 @@
 # Add your code here
+
+def run(songs)
+  x = 1
+  numarr = [*1..9]
+  found = false
+  while x > 0
+
+    input = gets.strip
+    if input == "exit"
+      puts "Goodbye!"
+      return
+    elsif input == "help"
+    help
+    elsif input == "play"
+      puts "Please enter song name or number:"
+      secondInput = gets.strip
+
+      numarr.each do |ele|
+        if ele.to_s == secondInput
+        puts "Playing " + songs[secondInput.to_i]
+        found = true
+        end
+      end
+
+      if found == false
+        puts "Invalid input, please try again"
+      else
+        found = false
+      end
+    elsif input == "list"
+    list(songs)
+    end
+  end
+
+
+end
+
+def help
+
+puts "I accept the following commands:"
+puts "- help : displays this help message"
+puts "- list : displays a list of songs you can play"
+puts "- play : lets you choose a song to play"
+puts "- exit : exits this program"
+
+end
+
+def list(arr)
+
+arr.each_with_index do |ele, ind|
+indadjusted = ind + 1
+puts indadjusted.to_s + ". " + ele
+end
+
+
+end
